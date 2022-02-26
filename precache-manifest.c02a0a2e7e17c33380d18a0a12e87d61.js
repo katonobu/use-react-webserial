943137cb4afa81281198f2ea13b5b5de38b6f860
katonobu <katonobu4649@gmail.com>
2022-02-26 14:56:39 +0900

    Updates

---
+++ precache-manifest.c02a0a2e7e17c33380d18a0a12e87d61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18937dd763d5e32fd1d514fb45bff891",
+    "url": "/use-react-webserial/index.html"
+  },
+  {
+    "revision": "37f886631b3b957352c9",
+    "url": "/use-react-webserial/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "119441a123182ac73b83",
+    "url": "/use-react-webserial/static/js/2.8705521b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/use-react-webserial/static/js/2.8705521b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6effd116570953a9b278",
+    "url": "/use-react-webserial/static/js/3.996ea8da.chunk.js"
+  },
+  {
+    "revision": "f71d7e2dfad1a9835fd5",
+    "url": "/use-react-webserial/static/js/4.20b03f96.chunk.js"
+  },
+  {
+    "revision": "b177f19c9689f64bf80f45881f27733e",
+    "url": "/use-react-webserial/static/js/4.20b03f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f886631b3b957352c9",
+    "url": "/use-react-webserial/static/js/main.33afb481.chunk.js"
+  },
+  {
+    "revision": "1892176f30fab07ee4a6",
+    "url": "/use-react-webserial/static/js/runtime-main.17602412.js"
+  }
+]);
